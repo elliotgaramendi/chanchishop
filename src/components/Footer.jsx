3f37492b81d0dd0d14cb9abe5c842b54d3d41489
elliotgaramendi <elliotgaramendi@gmail.com>
2025-05-07 20:45:29 -0500

    Fix label attribute in Footer component for proper accessibility

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -37,7 +37,7 @@ const Footer = () => {
             <form>
               <h5>Subscribe to our newsletter</h5>
               <p>Monthly digest of what's new and exciting from us.</p>
-              <div className="d-flex flex-column flex-sm-row w-100 gap-2"> <label for="newsletter1" className="visually-hidden">Email
+              <div className="d-flex flex-column flex-sm-row w-100 gap-2"> <label htmlFor="newsletter1" className="visually-hidden">Email
                 address</label> <input id="newsletter1" type="email" className="form-control" placeholder="Email address" />
                 <button className="btn btn-primary" type="button">Subscribe</button> </div>
             </form>
