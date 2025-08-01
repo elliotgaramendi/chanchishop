77e44faebe888052e0025ec70321908df4e9faf0
elliotgaramendi <elliotgaramendi@gmail.com>
2025-07-31 20:26:21 -0500

    feat: implement product fetching service and integrate in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,14 @@
+import { useEffect } from "react";
+import { getProducts } from "./services/products.service";
+
 function App() {
+
+  useEffect(() => {
+    (async () => {
+      const products = await getProducts();
+      console.log(products);
+    })();
+  }, []);
 
   return (
     <>
