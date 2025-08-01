3240657f1cb4d71687497305797cbf5a6c22d249
elliotgaramendi <elliotgaramendi@gmail.com>
2025-07-31 20:41:14 -0500

    feat: render data of fetching

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getProducts } from "./services/products.service";
 
 function App() {
+  const [products, setProducts] = useState([]);
 
   useEffect(() => {
     (async () => {
       const products = await getProducts();
-      console.log(products);
+      setProducts(products);
     })();
   }, []);
 
   return (
-    <>
-      Chanchishop
-    </>
+    <ul>
+      {products.map((product) => (
+        <li key={product.id}>
+          <h2>{product.title}</h2>
+          <p>{product.description}</p>
+          <p>Price: ${product.price}</p>
+        </li>
+      ))}
+    </ul>
   )
 }
 
--- src/services/products.service.js
+++ src/services/products.service.js
@@ -2,7 +2,6 @@ export const getProducts = async () => {
   try {
     const response = await fetch('https://fakestoreapi.com/products');
     const data = await response.json();
-    console.log(data);
     return data;
   } catch (error) {
     console.error('Error fetching products:', error);
