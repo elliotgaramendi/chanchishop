ed429cf195b635d0eccf19a2b32651e13d51f523
elliotgaramendi <elliotgaramendi@gmail.com>
2025-08-07 21:19:02 -0500

    feat: update ProductsList

--- src/components/modules/ProductsList.jsx
+++ src/components/modules/ProductsList.jsx
@@ -1,37 +1,23 @@
-import StarRating from "../elements/StarRating";
+import { useStore } from "../../store/products.store";
+import ProductCard from "../components/ProductCard";
 
 const ProductsList = ({ data, setShoppingCart }) => {
+  const count = useStore((state) => state.count);
+
   return (
     <section className="py-5">
       <div className="container">
-        <h2>Products</h2>
+        <div className="d-flex">
+          <h2 className="position-relative">
+            Products
+            <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+              {count}
+              <span className="visually-hidden">unread messages</span>
+            </span>
+          </h2>
+        </div>
         <div className="row row-cols-1 row-cols-md-3 g-4">
-          {data.map((item) => {
-            const { id, title, price, category, image, rating } = item;
-            return (
-              <div key={id} className="col">
-                <div className="card h-100">
-                  <img src={image} alt={title} className="object-fit-contain" height={200} />
-                  <div className="card-body d-flex flex-column">
-                    <h6 className="card-subtitle mb-2 text-body-secondary">{category}</h6>
-                    <h5 className="card-title">{title}</h5>
-                    <div className="d-flex align-items-center gap-1">
-                      <StarRating rating={rating.rate} />
-                      <small>({rating.count})</small>
-                    </div>
-                    <h5 className="card-title text-primary fw-bold">${price}</h5>
-                    <button
-                      href={image}
-                      className="btn btn-primary mt-auto"
-                      onClick={() => setShoppingCart((prev) => [...prev, item])}
-                    >
-                      Add to Cart
-                    </button>
-                  </div>
-                </div>
-              </div>
-            )
-          })}
+          {data.map(item => <ProductCard key={item.id} data={item} setShoppingCart={setShoppingCart} />)}
         </div>
       </div>
     </section>
