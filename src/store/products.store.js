2c6536ef3583480a50d1c919544b6cc59f364fba
elliotgaramendi <elliotgaramendi@gmail.com>
2025-08-07 21:53:16 -0500

    feat: refactor  store

--- src/store/products.store.js
+++ src/store/products.store.js
@@ -1,8 +1,23 @@
 import { create } from 'zustand';
+import { getProducts } from '../services/products.service';
 
-export const useStore = create((set) => ({
-  count: 1,
-  inc: () => {
-    set((state) => ({ count: state.count + 1 }))
+export const useProductsStore = create((set) => ({
+  loading: false,
+  error: null,
+  products: [],
+  fetchProducts: async () => {
+    try {
+      set({ loading: true, error: null });
+      const data = await getProducts();
+      set({ products: data });
+    } catch (error) {
+      set({ error: error.message });
+    } finally {
+      set({ loading: false });
+    }
   },
+  shoppingCart: [],
+  setShoppingCart: (shoppingCart) => {
+    set({ shoppingCart });
+  }
 }));
