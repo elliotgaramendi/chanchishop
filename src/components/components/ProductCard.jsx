f11c0cd2fe59388f0c21cd08e366489a4f9e6aaa
elliotgaramendi <elliotgaramendi@gmail.com>
2025-08-07 21:18:53 -0500

    feat: create ProductCard

---
+++ src/components/components/ProductCard.jsx
@@ -0,0 +1,33 @@
+import { useStore } from "../../store/products.store";
+import StarRating from "../elements/StarRating";
+
+const ProductCard = ({ data, setShoppingCart }) => {
+  const inc = useStore((state) => state.inc);
+
+  const { title, price, category, image, rating } = data;
+  return (
+    <div className="col">
+      <div className="card h-100">
+        <img src={image} alt={title} className="object-fit-contain" height={200} />
+        <div className="card-body d-flex flex-column">
+          <h6 className="card-subtitle mb-2 text-body-secondary">{category}</h6>
+          <h5 className="card-title">{title}</h5>
+          <div className="d-flex align-items-center gap-1">
+            <StarRating rating={rating.rate} />
+            <small>({rating.count})</small>
+          </div>
+          <h5 className="card-title text-primary fw-bold">${price}</h5>
+          <button
+            href={image}
+            className="btn btn-primary mt-auto"
+            onClick={() => {setShoppingCart((prev) => [...prev, data]); inc();}}
+          >
+            Add to Cart
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ProductCard;
